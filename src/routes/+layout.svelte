e3a51ab2369625e2ac7695e8e00f781c80ffa67c
Vanessa Bernabe <vbernabemfa@cenic.org>
2025-03-13 21:30:14 -0700

    Add Vuln5

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,13 @@
 	import PreloadingIndicator from './PreloadingIndicator.svelte';
 
 	const { children } = $props();
+
+	let userInput = "";
+
+	// Function to update user input
+	function updateInput(event) {
+		userInput = event.target.value;
+	}
 </script>
 
 {#if $navigating}
@@ -15,3 +22,7 @@
 <main>
 	{@render children()}
 </main>
+
+<input type="text" placeholder="Enter text" oninput={updateInput} />
+<!-- 🚨 XSS Vulnerability: Directly injecting unescaped user input -->
+<p>Message: {@html userInput}</p>
